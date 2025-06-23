8c3c7b8d0fb9fb36b95598e7f943f0f479c1c89e
Seokjin Lee <seokjin.dev@gmail.com>
2025-06-23 14:32:38 +0900

    first commit

---
+++ src/main/java/io/fundy/fundyserver/FundyServerApplication.java
@@ -0,0 +1,13 @@
+package io.fundy.fundyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundyServerApplication.class, args);
+    }
+
+}
