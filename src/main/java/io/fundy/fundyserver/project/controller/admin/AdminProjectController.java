0944234fe1f306e9f86968b4b2dcb95673b5ffd2
SeokGeunCho <choseokgeun@gmail.com>
2025-06-26 09:15:48 +0900

    ãpc 환경 git hub ì원격 브랜치 연결 확인용 커밋 및 푸시

--- src/main/java/io/fundy/fundyserver/project/controller/admin/AdminProjectController.java
+++ src/main/java/io/fundy/fundyserver/project/controller/admin/AdminProjectController.java
@@ -1,13 +1,13 @@
-package io.fundy.fundyserver.project.controller.admin;
-
-import io.fundy.fundyserver.notification.dto.admin.AdminProjectRequestDto;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-//@PostMapping("/admin/projects/status")
-//public ResponseEntity<Void> updateProjectStatus(@RequestBody AdminProjectRequestDto dto) {
-//    projectService.updateStatus(dto.getProjectId(), dto.getProductStatus());
-//    return ResponseEntity.ok().build();
-//}
-
+//package io.fundy.fundyserver.project.controller.admin;
+//
+//import io.fundy.fundyserver.notification.dto.admin.AdminProjectRequestDto;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+////@PostMapping("/admin/projects/status")
+////public ResponseEntity<Void> updateProjectStatus(@RequestBody AdminProjectRequestDto dto) {
+////    projectService.updateStatus(dto.getProjectId(), dto.getProductStatus());
+////    return ResponseEntity.ok().build();
+////}
+//
--- src/main/java/io/fundy/fundyserver/project/controller/admin/AdminUserController.java
+++ src/main/java/io/fundy/fundyserver/project/controller/admin/AdminUserController.java
@@ -1,30 +1,30 @@
-package io.fundy.fundyserver.project.controller.admin;
-
-
-import io.fundy.fundyserver.notification.dto.admin.AdminUserRequestDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-//@RestController
-//@RequestMapping("/admin/users")
-//@RequiredArgsConstructor
-//public class AdminUserController {
+//package io.fundy.fundyserver.project.controller.admin;
 //
-//    private final UserService userService;
 //
-//    @PostMapping("/status")
-//    public ResponseEntity<Void> updateUserStatus(@RequestBody AdminUserRequestDto dto) {
-//        // 요청이 "BAN"이면 내부적으로 "BANNED"로 매핑
-//        String status = "BAN".equalsIgnoreCase(dto.getUserStatus())
-//                ? "BANNED"
-//                : dto.getUserStatus().toUpperCase();
+//import io.fundy.fundyserver.notification.dto.admin.AdminUserRequestDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 //
-//        userService.updateUserStatus(dto.getUserId(), status);
-//        return ResponseEntity.ok().build();
-//    }
-//}
-
+////@RestController
+////@RequestMapping("/admin/users")
+////@RequiredArgsConstructor
+////public class AdminUserController {
+////
+////    private final UserService userService;
+////
+////    @PostMapping("/status")
+////    public ResponseEntity<Void> updateUserStatus(@RequestBody AdminUserRequestDto dto) {
+////        // 요청이 "BAN"이면 내부적으로 "BANNED"로 매핑
+////        String status = "BAN".equalsIgnoreCase(dto.getUserStatus())
+////                ? "BANNED"
+////                : dto.getUserStatus().toUpperCase();
+////
+////        userService.updateUserStatus(dto.getUserId(), status);
+////        return ResponseEntity.ok().build();
+////    }
+////}
+//
