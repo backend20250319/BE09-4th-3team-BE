03c132b33546c4813f18bfcaf91440bb494bd194
choseokgeun <choseokgeun@gmail.com>
2025-07-07 15:48:05 +0900

    ãChange the maximum number of ID(11->20) and password(15->20) entries

--- src/main/java/io/fundy/fundyserver/register/dto/UserRequestDTO.java
+++ src/main/java/io/fundy/fundyserver/register/dto/UserRequestDTO.java
@@ -18,12 +18,12 @@ public class UserRequestDTO {
 
     @NotBlank(message = "아이디는 필수 입력 항목입니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영문 및 숫자만 사용할 수 있습니다.")
-    @Size(min = 5, max = 10, message = "※ 아이디는 영문 5-10자까지 입력 가능합니다.")
+    @Size(min = 5, max = 20, message = "※ 아이디는 영문 + 숫자 5~20자여야 합니다.")
     private String userId;
 
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&]).{8,30}$", message = "비밀번호는 문자+숫자+특수문자를 포함해야 합니다.")
-    @Size(min = 8, max = 15, message = "※ 비밀번호는 영문과 특수문자를 혼용하여 8-15자를 입력해 주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&]).{8,20}$", message = "비밀번호는 문자+숫자+특수문자를 포함해야 합니다.")
+    @Size(min = 8, max = 20, message = "※ 비밀번호는 영문 대소문자+숫자+특수문자 포함 8~20자여야 합니다.")
     private String password;
 
     @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
@@ -36,8 +36,8 @@ public class UserRequestDTO {
     private String email;
 
     @NotBlank(message = "전화번호는 필수 입력 항목입니다.")
-    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호는 000-0000-0000 형식으로 입력해주세요.")
-    @Size(max = 15, message = "※ 000-0000-0000 형식으로 입력해주세요.")
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "유효한 전화번호를 입력해주세요.(예:010-1234-1234)")
+    @Size(max = 15, message = "※ 유효한 전화번호를 입력해주세요.(예:010-1234-1234)")
     private String phone;
 
     @Size(max = 200, message = "※ 주소는 최대 200자까지 입력 가능합니다.")
